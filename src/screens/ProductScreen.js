81f0dfdbba5b59c0e62d334dc533a345d736c524
remi95 <remimafat@gmail.com>
2019-02-17 20:29:07 +0100

    Normalize product object and display on screen

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Image, Text, FlatList} from 'react-native';
 import OpenClient from "../clients/OpenFactClient";
+import colors from "../constants/colors";
 
 class ProductScreen extends Component {
 
@@ -12,17 +13,83 @@ class ProductScreen extends Component {
         }
     }
 
+    _keyExtractor = (item, index) => (item + index).toString();
+
     render() {
+        let { product } = this.state;
+
         return (
             <View>
+            {
+                product !== null
+                ?
+                    <View>
+                        <View>
+                            <Image source={{uri: product.image}} style={styles.image} />
+                            <Text>{ product.name }</Text>
+                            <Text>{ product.brand }</Text>
+                            <Text>{ product.weight }</Text>
+                        </View>
 
+                        <Text>{ product.description }</Text>
+
+                        <View>
+                            <Text>{ product.nutrient_grade }</Text>
+                            <Text>Allergènes : { product.allergens }</Text>
+                            <Text>Ingrédients :</Text>
+                            <FlatList
+                                data={product.ingredients}
+                                keyExtractor={this._keyExtractor}
+                                renderItem={ ({item}) => <Text>{`\u2022 ${item}`}</Text>}
+                            />
+                        </View>
+
+                        <View>
+                            <View>
+                                {
+                                    product.nutriments.map(nutriment =>
+                                        <Text key={`${nutriment.name}_name`}>{nutriment.name}</Text>
+                                    )
+                                }
+                            </View>
+                            <View>
+                                {
+                                    product.nutriments.map(nutriment =>
+                                        <Text key={`${nutriment.name}_value`}>
+                                            {nutriment.value} {nutriment.unit}
+                                        </Text>
+                                    )
+                                }
+                            </View>
+                        </View>
+                    </View>
+
+                :   null
+            }
             </View>
         )
     }
 
-    componentDidMount() {
-        OpenClient.getProduct(this.props.code);
+    async componentDidMount() {
+        let product = await OpenClient.getProduct(this.props.code);
+
+        if (product.status) {
+            let data = await OpenClient.normalizeProduct(product.data);
+            console.log(data)
+            this.setState({ product: data });
+        }
+        // TODO: Manage if API doesn't respond correctly.
     }
 }
+
+const styles = StyleSheet.create({
+    image: {
+        height: 50,
+        width: 50,
+
+    },
+
+});
+
 
 export default ProductScreen;
