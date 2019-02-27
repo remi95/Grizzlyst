01039622f4ed0322c1cc69e0a8411af4b7b5989d
arnaud <arnaud_lafon@hotmail.fr>
2019-02-27 13:39:31 +0100

    invitationscreen beginning

--- src/screens/user/InvitationScreen.js
+++ src/screens/user/InvitationScreen.js
@@ -1,15 +1,44 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button, FlatList} from 'react-native';
+import GrizzlystClient from '../../clients/GrizzlystClient';
+import { connect } from 'react-redux';
 
 class InvitationScreen extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            invitations: []
+        }
+    }
+
+    async componentDidMount() {
+        const invitations = await GrizzlystClient.get(`users/me/invitations`);
+        this.setState({
+            invitations
+        });
+    }
+
+    renderItem({item}) {
+        return (
+            <View>
+                <Text>{item.group.name}</Text>
+                <Button title='Accepter' onPress={ () => null } />
+                <Button title='Refuser' onPress={ () => null } />
+            </View>
+        )
     }
 
     render() {
         return (
             <View>
                 <Text>Hello</Text>
+                <FlatList
+                    data={ this.state.invitations }
+                    renderItem={({item}) => (
+                        this.renderItem({item})
+                    )}
+                    keyExtractor={(item, index) => index.toString()}/>
             </View>
         )
     }
